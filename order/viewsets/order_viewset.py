b061fc218101240c914440b76e1c2f7361ad5603
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2024-04-10 14:19:43 -0300

    autenticação via token

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,7 +1,10 @@
-from order.models import Order
-from order.serializers import OrderSerializer
 from rest_framework.viewsets import ModelViewSet
 
+from order.models import Order
+from order.serializers import OrderSerializer
+
+
 class OrderViewSet(ModelViewSet):
+
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().order_by("id")
