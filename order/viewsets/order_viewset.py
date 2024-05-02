74b6bc71da80026febb2a57123c782528af5f814
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2024-05-02 10:03:58 -0300

    teste 3

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -5,6 +5,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-class OrderViewSets(ModelViewSet):
+class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by('id')
