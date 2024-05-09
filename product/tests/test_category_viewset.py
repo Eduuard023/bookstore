1a4951d3ff628d98911b64b27d2e7b6a297816e4
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2024-05-09 12:12:03 -0300

    teste 7

--- product/tests/test_category_viewset.py
+++ product/tests/test_category_viewset.py
@@ -1,7 +1,7 @@
 import json
 
-from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
 from django.urls import reverse
 
@@ -16,26 +16,22 @@ class CategoryViewSet(APITestCase):
         self.category = CategoryFactory(title='books')
 
     def test_get_all_category(self):
-        response = self.client.get(
-            reverse('category-list', kwargs={'version': 'v1'})
-        )
-
+        response = self.client.get(reverse('category-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         category_data = json.loads(response.content)
 
-        self.assertEqual(category_data[0]['title'], self.category.title)
+        self.assertEqual(category_data['results'][0]['title'], self.category.title)
 
     def test_create_category(self):
         data = json.dumps({
-            'title': 'technology'
+            'title': 'technology',
         })
 
         response = self.client.post(
-            reverse('category-list', kwargs={'version': 'v1'}),
+            reverse('category-list'),
             data=data,
             content_type='application/json'
         )
-        #import pdb; pdb.set_trace()
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
